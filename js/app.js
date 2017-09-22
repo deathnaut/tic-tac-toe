716b1bae0b0616dd35c49868eb13fe3f74fd055a
Karla Moreno <moreno_karla@live.com>
2017-09-22 12:06:59 -0500

    added scoreboard and box-click functionality

--- js/app.js
+++ js/app.js
@@ -5,13 +5,15 @@
 
 // });
 
+console.log('start tic-tac-toe game');
+
 var title = document.querySelector('h1');
 // xTurn = true;
-console.log(title);
 
 title.addEventListener('click', function(){
   // if (xTurn == true) {
   title.style.background = 'blue';
+  console.log('a change has been made..');
   // } else {
     // document.getElementById("box1A").innerHTML = "Goodbye World";
 });
@@ -21,4 +23,22 @@ var button = document.querySelector('button');
 
 button.addEventListener('click', function(){
   window.location.reload(true);
+});
+
+var x = 'x'
+var o = 'o'
+var entry = ''
+
+// var boardRow = [...document.querySelectorAll('.row')];
+var boardBox = [...document.querySelectorAll('.box')];
+var box = [...document.querySelectorAll('.col-md-4')];
+
+boardBox.forEach(function (clickBox) {
+  clickBox.addEventListener('click', function (){
+    entry = x;
+    box.innerHTML = entry;
+    console.log(entry);
+    clickBox.style.backgroundColor = 'white';
+
+  });
 });
