ab75c05af8f2baaef02c65052e559d6b59772daf
Karla Moreno <moreno_karla@live.com>
2017-09-22 10:09:28 -0500

    added js file/functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 // wait for the DOM to finish loading
-$(document).ready(function() {
+// $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+// });
+
+var title = document.querySelector("h1");
+// xTurn = true;
+console.log(title);
+
+title.addEventListener("click", function(){
+  // if (xTurn == true) {
+  title.style.background = 'blue';
+  // } else {
+    // document.getElementById("box1A").innerHTML = "Goodbye World";
 });
