bb27afbdfc3aa8e5a199f49fc965a322954cbb05
Karla Moreno <moreno_karla@live.com>
2017-09-25 02:16:49 -0500

    added rick dance, cleaned up code a tiny bit

--- js/app.js
+++ js/app.js
@@ -8,13 +8,12 @@
 console.log('start tic-tac-toe game');
 
 var title = document.querySelector('h1');
-// xTurn = true;
 
-title.addEventListener('click', function(){
-  // if (xTurn == true) {
-  title.style.background = 'blue';
-  console.log('a change has been made..');
-});
+// TEST stuff
+// title.addEventListener('click', function(){
+//   title.style.background = 'blue';
+//   console.log('a change has been made..');
+// });
 
 
 var turnCounter = 0;
@@ -26,16 +25,6 @@ var clickBoxValue = document.querySelector('value');
 var boardRow = [...document.querySelectorAll('.row')];
 var boardBox = [...document.querySelectorAll('.box')];
 var box = [...document.querySelectorAll('.col-md-4')];
-
-var a1 = document.querySelector('.row1 .box .a');
-var a2 = document.querySelector('.row1 .box .b');
-var a3 = document.querySelector('.row1 .box .c');
-var b1 = document.querySelector('.row2 .box .a');
-var b2 = document.querySelector('.row2 .box .b');
-var b3 = document.querySelector('.row2 .box .c');
-var c1 = document.querySelector('.row3 .box .a');
-var c2 = document.querySelector('.row3 .box .b');
-var c3 = document.querySelector('.row3 .box .c');
 
 
 boardBox.forEach(function (clickBox) {
@@ -49,9 +38,6 @@ boardBox.forEach(function (clickBox) {
         console.log(clickBox);
         console.log('box value is now true aka x')
         console.log('player x turn has been played');
-        // if ((win[1] === 'x') && (win[2] === 'x') && (win[3] === 'x')) {
-        //   alert('player x wins');
-        // }
       } else {
         turnCounter++;
         clickBox.innerHTML = o;
@@ -69,32 +55,7 @@ boardBox.forEach(function (clickBox) {
 });
 
 
-function checkIfWin () {
 
-}
-
-var row1 = [...document.querySelectorAll('.row1')];
-var row2 = [...document.querySelectorAll('.row2')];
-var row3 = [...document.querySelectorAll('.row3')];
-var rowA = [...document.querySelectorAll('.row .a')];
-var rowC = [...document.querySelectorAll('.row .c')];
-var rowB = [...document.querySelectorAll('.row .b')];
-
-function isSame(){
-  row1.forEach('.box', function (){
-    if (clickBoxValue === true){
-      alert('Player X wins.');
-    }
-  }
-)};
-//   row1 = document.querySelector('clickBoxValue = ')
-//   row2
-//   row3
-//   row a
-//   row b
-//   row c
-//   row1 a, row2 b, row3 c
-//   row1 c, row2 b, row3 a
 
 //button stuff
 var button = document.querySelector('button');
@@ -103,30 +64,88 @@ button.addEventListener('click', function(){
   window.location.reload(true);
 });
 
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 
+// PARTY MODE
 var party = document.querySelector('a');
-
 var partyStarted = document.querySelector('#partyStarted');
 
 party.addEventListener('click', function(){
     var body = document.querySelector('body');
-    // color = colors.shift();
-    // colors.push(color);
-    // body.style.backgroundColor = changeColors;
-    // function changeColors(){
-    //   for (var i = 0; i < colors.length; i++) {
-    //     title.style.color = colors[i];
-    //     setTimeout(changeColors, 1000);
-    //   }
-    // }
-    title.style.color = colors[0];
-    partyStarted.innerHTML = '<img id ="partyStarted" src="../tic-tac-toe/images/lisa-dancing.gif" alt = "lisa simpson dancing"></img>';
+
+    title.style.color = 'white';
+    partyStarted.innerHTML = '<img id ="partyStarted" src="../tic-tac-toe/images/rick-dance1.gif" alt = "lisa simpson dancing"></img>';
     body.style.backgroundImage = "url('../tic-tac-toe/images/partytime.gif')";
-    // boardBox.style.backgroundColor = 'rgba(0,0,0,0)';
     party.innerText = 'PARTY!';
     var audio = new Audio('400611__valo__trance-bass-and-drums-loop.mp3');
     audio.loop = true;
     audio.play();
-    console.log('i should change background & play music');
+    console.log('i should change background & play music and other stuff');
 });
+
+
+// -------------------------------------------------------------------
+
+// TRASH PILE aka code i tried to use but didn't work, may come back to
+
+
+// FOR CHANGING COLORS IN PARTY MODE
+
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+// boardBox.style.backgroundColor = 'rgba(0,0,0,0)';
+// color = colors.shift();
+// colors.push(color);
+// body.style.backgroundColor = changeColors;
+// function changeColors(){
+//   for (var i = 0; i < colors.length; i++) {
+//     title.style.color = colors[i];
+//     setTimeout(changeColors, 1000);
+//   }
+// }
+
+// FOR GAME LOGIC/TESTING TO SEE IF THERE IS A WIN
+
+// function isSame(){
+//   row1.forEach('.box', function (){
+//     if (clickBoxValue === true){
+//       alert('Player X wins.');
+//     }
+//   }
+// )};
+
+// FOR GAME LOGIC/SHOWING WIN POSSIBILITES
+
+// //   row1 = document.querySelector('clickBoxValue = ')
+// //   row2
+// //   row3
+// //   row a
+// //   row b
+// //   row c
+// //   row1 a, row2 b, row3 c
+// //   row1 c, row2 b, row3 a
+
+// MORE FAILED GAME LOGIC
+
+// function checkIfWin () {
+//
+// }
+//
+// // var row1 = [...document.querySelectorAll('.row1')];
+// // var row2 = [...document.querySelectorAll('.row2')];
+// // var row3 = [...document.querySelectorAll('.row3')];
+// // var rowA = [...document.querySelectorAll('.row .a')];
+// // var rowC = [...document.querySelectorAll('.row .c')];
+// // var rowB = [...document.querySelectorAll('.row .b')];
+
+// if ((win[1] === 'x') && (win[2] === 'x') && (win[3] === 'x')) {
+//   alert('player x wins');
+// }
+
+// var a1 = document.querySelector('.row1 .box .a');
+// var a2 = document.querySelector('.row1 .box .b');
+// var a3 = document.querySelector('.row1 .box .c');
+// var b1 = document.querySelector('.row2 .box .a');
+// var b2 = document.querySelector('.row2 .box .b');
+// var b3 = document.querySelector('.row2 .box .c');
+// var c1 = document.querySelector('.row3 .box .a');
+// var c2 = document.querySelector('.row3 .box .b');
+// var c3 = document.querySelector('.row3 .box .c');
