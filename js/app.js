179c77bd341bc8263d6c6c139fdb65f49e743ddd
Karla Moreno <moreno_karla@live.com>
2017-09-25 12:16:53 -0500

    FINALLY GOT MY GAME LOGIC TO WORK!!!!

--- js/app.js
+++ js/app.js
@@ -23,32 +23,38 @@ var boardRow = [...document.querySelectorAll('.row')];
 var boardBox = [...document.querySelectorAll('.box')];
 var box = [...document.querySelectorAll('.col-md-4')];
 
+var winner = null;
 
-var a1 = document.querySelector('.row1 .box .a');
-var a2 = document.querySelector('.row1 .box .b');
-var a3 = document.querySelector('.row1 .box .c');
-var b1 = document.querySelector('.row2 .box .a');
-var b2 = document.querySelector('.row2 .box .b');
-var b3 = document.querySelector('.row2 .box .c');
-var c1 = document.querySelector('.row3 .box .a');
-var c2 = document.querySelector('.row3 .box .b');
-var c3 = document.querySelector('.row3 .box .c');
+function checkIfWin(){
+  console.log('run function checkIfWin');
+  var win1AcrossX = [...document.querySelectorAll('.row1 > img#x')];
+  console.log(win1AcrossX.length);
+  var win2AcrossX = [...document.querySelectorAll('.row2 > img#x')];
+  var win3AcrossX = [...document.querySelectorAll('.row3 > img#x')];
+  var win4UpDownX = [...document.querySelectorAll('.colA > img#x')];
+  var win5UpDownX = [...document.querySelectorAll('.colB > img#x')];
+  var win6UpDownX = [...document.querySelectorAll('.colC > img#x')];
+  var win7DiagX = [...document.querySelectorAll('.diag1 > img#x')];
+  var win8DiagX = [...document.querySelectorAll('.diag2 > img#x')];
 
-// var row1Win = [...document.querySelectorAll('row1 > img#xImg')];
-//
-// var j = 0;
-//
-// function checkIfWin(){
-//   // for (j = 0, j < turnCounter, j++) {
-//   //   if ( $( "#a1" ).attr( "value" ) == 'true' ) {
-//   //     console.log('a1 is x');
-//   //   } else {
-//   //     console.log('check your code');
-//   //   }
-//   if (row1Win.length === 3) {
-//       console.log('x wins');
-//   }
-// }
+  var win1AcrossO = [...document.querySelectorAll('.row1 > img#o')];
+  var win2AcrossO = [...document.querySelectorAll('.row2 > img#o')];
+  var win3AcrossO = [...document.querySelectorAll('.row3 > img#o')];
+  var win4UpDownO = [...document.querySelectorAll('.colA > img#o')];
+  var win5UpDownO = [...document.querySelectorAll('.colB > img#o')];
+  var win6UpDownO = [...document.querySelectorAll('.colC > img#o')];
+  var win7DiagO = [...document.querySelectorAll('.diag1 > img#o')];
+  var win8DiagO = [...document.querySelectorAll('.diag2 > img#o')];
+
+  if (win1AcrossX.length === 3 || win2AcrossX.length === 3 || win3AcrossX.length === 3 || win4UpDownX.length === 3 || win5UpDownX.length === 3 || win6UpDownX.length === 3 || win7DiagX.length === 3 || win8DiagX.length === 3) {
+    console.log('win req met');
+    alert('PLAYER X WINS!');
+  } else if (win1AcrossO.length === 3 || win2AcrossO.length === 3 || win3AcrossO.length === 3 || win4UpDownO.length === 3 || win5UpDownO.length === 3 || win6UpDownO.length === 3 || win7DiagO.length === 3 || win8DiagO.length === 3) {
+    console.log('win req met');
+    alert('PLAYER O WINS!');
+  };
+  console.log('function end');
+};
 
 var turnCounter = 0;
 
@@ -61,23 +67,21 @@ boardBox.forEach(function (clickBox) {
         console.log(clickBox);
         console.log('box value is now true aka x');
         console.log('player x turn has been played');
-        // checkIfWin();
       } else if (turnEven === 1) {
         clickBox.innerHTML = o;
         console.log(clickBox);
         console.log('box value is now false aka o');
         console.log('player o turn has been played');
-        // checkIfWin();
       }
+      checkIfWin();
       turnCounter++;
     } else {
         alert('box is taken');
       }
       clickBox.style.backgroundColor = 'white';
-    // if (turnCounter === 9) {
-    //     alert('GAME OVER: DRAW.');
-    //   }
-    // need 'box is taken' func
+    if (turnCounter === 9) {
+      alert('GAME OVER: DRAW.');
+    }
   });
 });
 
